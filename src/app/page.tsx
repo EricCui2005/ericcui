98498da379847f54203f5dbf5eb954744c700f84
ericcui <ericcui2005@gmail.com>
2024-12-13 19:37:46 -0800

    Basic parallax configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+'use client';
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+
 export default function Home() {
+  const SlowAndFast = () => (
+    <>
+      <Parallax speed={-5}>
+        <div className="slow">Hello</div>
+      </Parallax>
+      <Parallax speed={5}>
+        <div className="fast">Hello</div>
+      </Parallax>
+    </>
+  );
+
   return (
     <>
-      Hello
-      <div>Testing this thing</div>
+      {/* <ParallaxProvider scrollAxis="vertical">
+        <SlowAndFast></SlowAndFast>
+      </ParallaxProvider> */}
+      <div className="h-full border border-black">Testing</div>
     </>
   );
 }
