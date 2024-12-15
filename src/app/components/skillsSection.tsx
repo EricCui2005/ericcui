7b752bd2bfd03c2af79a7bf76120bdafee098c52
ericcui <ericcui2005@gmail.com>
2024-12-15 15:59:50 -0500

    Skills section positioning change

--- src/app/components/skillsSection.tsx
+++ src/app/components/skillsSection.tsx
@@ -5,9 +5,9 @@ import DevToolsCard from './devToolsCard';
 export default function SkillsSection() {
   return (
     <>
-      <div className="relative flex h-[110vh] w-full flex-col items-center justify-center">
-        <h1 className="absolute top-[140px] text-[36px] font-bold text-white">Skills</h1>
-        <div className="absolute top-[215px] flex flex-row gap-8">
+      <div className="debug relative flex h-[100vh] w-full flex-col items-center justify-center">
+        <h1 className="absolute top-[110px] text-[36px] font-bold text-white">Skills</h1>
+        <div className="absolute top-[200px] flex flex-row gap-8">
           <LanguagesCard title="Languages" />
           <TechnologiesCard title="Technologies" />
           <DevToolsCard title="Developer Tools" />
