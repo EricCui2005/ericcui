afe9c9feb49216f626a625ea03d033994ae3f4bc
ericcui <ericcui2005@gmail.com>
2024-12-15 02:52:23 -0500

    removed debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       </div>
       <div className="flex h-[200vh] flex-col items-center">
         <AboutSection />
-        <div className="debug relative flex h-[125vh] w-full flex-col items-center justify-center">
+        <div className="relative flex h-[125vh] w-full flex-col items-center justify-center">
           <h1 className="absolute top-[150px] text-[36px] font-bold text-white">Skills</h1>
           <div className="flex flex-row gap-8">
             <SkillCard title="Languages" />
