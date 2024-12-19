09b5878f7c580d9539eeb4a729da644fe6e41cf8
ericcui <ericcui2005@gmail.com>
2024-12-19 12:57:00 -0500

    Contact section spacing

--- src/app/components/contactSection.tsx
+++ src/app/components/contactSection.tsx
@@ -3,26 +3,26 @@ import ContactSubsection from './contactSubsection';
 export default function ContactSection() {
   return (
     <>
-      <div className="bg-darkestBlue relative flex h-[35vh] w-full flex-row items-center justify-center">
-        <div className="flex w-5/6 flex-row justify-center gap-48">
+      <div className="relative flex h-[35vh] w-full flex-row items-center justify-center bg-darkestBlue">
+        <div className="flex w-5/6 flex-row justify-center gap-64">
           <ContactSubsection title="Socials">
             <a
               href="https://www.linkedin.com/in/eric-cui-003154233/"
-              className="text-offWhite text-[14px] hover:underline"
+              className="text-[14px] text-offWhite hover:underline"
               target="_blank"
             >
               LinkedIn
             </a>
             <a
               href="https://github.com/EricCui2005"
-              className="text-offWhite text-[14px] hover:underline"
+              className="text-[14px] text-offWhite hover:underline"
               target="_blank"
             >
               GitHub
             </a>
             <a
               href="https://x.com/EricCui29"
-              className="text-offWhite text-[14px] hover:underline"
+              className="text-[14px] text-offWhite hover:underline"
               target="_blank"
             >
               X
@@ -31,13 +31,13 @@ export default function ContactSection() {
           <ContactSubsection title="Emails">
             <a
               href="mailto:ericcui.career@gmail.com"
-              className="text-offWhite text-[14px] hover:underline"
+              className="text-[14px] text-offWhite hover:underline"
             >
               ericcui.career@gmail.com (work)
             </a>
             <a
               href="mailto:ericcui@stanford.edu"
-              className="text-offWhite text-[14px] hover:underline"
+              className="text-[14px] text-offWhite hover:underline"
             >
               ericcui@stanford.edu (school)
             </a>
@@ -45,7 +45,7 @@ export default function ContactSection() {
           <ContactSubsection title="Resume">
             <a
               href="https://drive.google.com/file/d/1lzvMAbGZpkWtzr3VYtSCrzv1w0cpZ0lV/view?usp=sharing"
-              className="text-offWhite text-[14px] hover:underline"
+              className="text-[14px] text-offWhite hover:underline"
               target="_blank"
             >
               Eric Cui Resume.pdf
