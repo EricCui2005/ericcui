6fffc6082885b30459d912306a90e2e93fea695f
ericcui <ericcui2005@gmail.com>
2024-12-25 22:08:36 -0500

    About section refactor

--- src/app/sections/aboutSection.tsx
+++ src/app/sections/aboutSection.tsx
@@ -3,13 +3,13 @@ import RoundedImage from '../components/roundedImage';
 export default function AboutSection() {
   return (
     <>
-      <div className="relative flex h-[600px] w-full flex-row items-center justify-center bg-offWhite">
-        <div className="absolute left-[100px]">
+      <div className="relative flex h-[70vh] w-full flex-row items-center justify-center gap-[10vw] bg-offWhite">
+        <div>
           <RoundedImage image="/Eric.jpeg" dim={350} />
         </div>
-        <div className="absolute right-[100px] flex h-[350px] w-[800px] flex-col items-center justify-center">
-          <h1 className="mb-4 text-[28px] font-bold">About</h1>
-          <p className="text-[20px]">
+        <div className="flex h-[35vh] w-[53vw] flex-col items-center justify-center">
+          <h1 className="mb-[2vh] text-2xl font-bold">About</h1>
+          <p className="text-xl">
             Hi there! I'm Eric Cui, a Computer Science student at Stanford University specializing
             in Artificial Intelligence. I'm passionate about creating innovative solutions at the
             intersection of full-stack development, deep learning, and cutting-edge AI research. My
