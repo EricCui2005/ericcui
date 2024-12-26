cf0b6239643d3b7b589bb2c0e642b535c92d2e33
ericcui <ericcui2005@gmail.com>
2024-12-26 17:51:05 -0500

    Minimal about section refactor

--- src/app/sections/aboutSection.tsx
+++ src/app/sections/aboutSection.tsx
@@ -1,14 +1,9 @@
-import RoundedImage from '../components/roundedImage';
-
 export default function AboutSection() {
   return (
     <>
-      <div className="relative flex h-[70vh] w-full flex-row items-center justify-center gap-[10vw] bg-offWhite">
-        <div>
-          <RoundedImage image="/Eric.jpeg" dim={350} />
-        </div>
-        <div className="flex h-[35vh] w-[53vw] flex-col items-center justify-center">
-          <h1 className="mb-[2vh] text-2xl font-bold">About</h1>
+      <div className="debug relative flex h-auto w-full flex-col-reverse items-center justify-center bg-offWhite px-12 py-36 md:flex-row">
+        <div className="flex h-auto w-[80%] flex-col justify-center px-12">
+          <h1 className="mb-4 text-2xl font-bold">About</h1>
           <p className="text-xl">
             Hi there! I'm Eric Cui, a Computer Science student at Stanford University specializing
             in Artificial Intelligence. I'm passionate about creating innovative solutions at the
@@ -17,6 +12,9 @@ export default function AboutSection() {
             processing systems, and managing robust API services. I'm always eager to tackle
             challenging projects and collaborate with others to make an impact. Let's connect!
           </p>
+        </div>
+        <div className="flex h-auto w-96 flex-col items-center justify-center">
+          <img src="/Eric.jpeg" alt="Eric" className="rounded-full" />
         </div>
       </div>
     </>
