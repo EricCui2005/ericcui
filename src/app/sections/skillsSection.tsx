2e9e835e08bb2e5a6c3aa382494cdc0b7ed84481
ericcui <ericcui2005@gmail.com>
2024-12-25 22:11:21 -0500

    Skills container refactor

--- src/app/sections/skillsSection.tsx
+++ src/app/sections/skillsSection.tsx
@@ -6,8 +6,8 @@ export default function SkillsSection() {
   return (
     <>
       <div className="relative flex h-[100vh] w-full flex-col items-center justify-center">
-        <h1 className="absolute top-[120px] text-[36px] font-bold text-white">Skills</h1>
-        <div className="absolute top-[200px] flex flex-row gap-8">
+        <h1 className="absolute top-[14%] text-3xl font-bold text-white">Skills</h1>
+        <div className="absolute top-[22%] flex flex-row gap-[2vw]">
           <LanguagesCard title="Languages" />
           <TechnologiesCard title="Technologies" />
           <DevToolsCard title="Developer Tools" />
