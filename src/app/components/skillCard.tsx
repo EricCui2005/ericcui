8da8ecc72aae3c4b79733406f41cbeb244e87259
ericcui <ericcui2005@gmail.com>
2025-01-06 22:54:49 -0800

    mobile-first refactoring

--- src/app/components/skillCard.tsx
+++ src/app/components/skillCard.tsx
@@ -1,9 +1,9 @@
 export default function SkillCard({ title, children }) {
   return (
     <>
-      <div className="flex h-[65vh] w-[30vw] flex-col items-center rounded-3xl bg-lightBlue pt-[1.5vh] drop-shadow-xl">
-        <h1 className="mt-1 text-[28px] text-white">{title}</h1>
-        <div className="mt-4 flex h-[75%] w-[75%] flex-wrap content-start justify-center gap-x-2 gap-y-2">
+      <div className="flex h-auto w-[30vw] flex-col items-center gap-6 rounded-3xl bg-lightBlue py-6 drop-shadow-xl">
+        <h1 className="text-2xl text-white">{title}</h1>
+        <div className="flex h-auto w-[75%] flex-wrap content-start justify-center gap-x-2 gap-y-2">
           {children}
         </div>
       </div>
