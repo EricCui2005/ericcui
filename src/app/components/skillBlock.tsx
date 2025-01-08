9e0d4e3147376ec1b7dc9a03c52c73894070cee3
ericcui <ericcui2005@gmail.com>
2025-01-07 21:17:42 -0800

    skillBlock sizing refactor

--- src/app/components/skillBlock.tsx
+++ src/app/components/skillBlock.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function SkillBlock({ label, img }) {
   return (
     <>
-      <div className="flex h-[12vh] w-[7vw] flex-col items-center justify-center gap-[1vh] rounded-xl bg-lightBlueHover">
+      <div className="flex h-28 w-28 flex-col items-center gap-4 rounded-xl bg-lightBlueHover p-4">
         <h1 className="text-sm text-white">{label}</h1>
         <Image src={img} height={40} width={40} alt={label} />
       </div>
--- src/app/components/skillCard.tsx
+++ src/app/components/skillCard.tsx
@@ -1,9 +1,9 @@
 export default function SkillCard({ title, children }) {
   return (
     <>
-      <div className="flex h-auto w-full flex-col items-center gap-6 rounded-3xl bg-lightBlue py-6 drop-shadow-xl">
+      <div className="flex h-[36rem] w-full flex-col items-center gap-6 rounded-3xl bg-lightBlue py-6 drop-shadow-xl">
         <h1 className="text-2xl text-white">{title}</h1>
-        <div className="flex h-auto w-[75%] flex-wrap content-start justify-center gap-x-2 gap-y-2">
+        <div className="flex h-full w-full flex-wrap content-start justify-center gap-x-2 gap-y-2 p-4">
           {children}
         </div>
       </div>
