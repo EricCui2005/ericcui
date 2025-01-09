6562121b8d56d7c4d53ed55d2e4ca0a8f9e4bf65
ericcui <ericcui2005@gmail.com>
2025-01-09 13:14:14 -0800

    Fixed navbar not fitting

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,10 +1,9 @@
 import NavButton from './navButton';
-import DropdownButton from './dropdownButton';
 
 export default function NavBar() {
   return (
     <>
-      <div className="font-text fixed hidden h-16 w-auto flex-row items-center justify-center rounded-2xl bg-lightBlue px-2 drop-shadow-xl md:flex">
+      <div className="font-text fixed flex h-16 w-auto flex-row items-center justify-center rounded-2xl bg-lightBlue px-2 drop-shadow-xl md:h-16">
         <NavButton text="Home" ref="home" />
         <NavButton text="About" ref="about" />
         <NavButton text="Skills" ref="skills" />
--- src/app/components/navButton.tsx
+++ src/app/components/navButton.tsx
@@ -3,7 +3,7 @@ export default function NavButton({ text, ref }) {
     <>
       <a
         href={`#${ref}`}
-        className="text-md flex h-[75%] w-auto select-none flex-col items-center justify-center rounded-xl px-4 text-white transition duration-300 hover:bg-lightBlueHover"
+        className="flex h-[75%] w-auto select-none flex-col items-center justify-center rounded-xl px-2 text-sm text-white transition duration-300 hover:bg-lightBlueHover md:px-4 md:text-base"
       >
         <h1>{text}</h1>
       </a>
