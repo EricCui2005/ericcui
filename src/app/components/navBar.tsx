286977df75de436054e007766755a6bcb3c86f40
ericcui <ericcui2005@gmail.com>
2025-01-17 21:21:14 -0800

    Commenting

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -2,8 +2,9 @@ import NavButton from './navButton';
 
 export default function NavBar() {
   return (
+    // Comments
     <>
-      <div className="font-text fixed flex h-16 w-auto flex-row items-center justify-center rounded-2xl bg-lightBlue px-2 drop-shadow-xl md:h-16">
+      <div className="fixed flex h-16 w-auto flex-row items-center justify-center rounded-2xl bg-lightBlue px-2 font-text drop-shadow-xl md:h-16">
         <NavButton text="Home" ref="home" />
         <NavButton text="About" ref="about" />
         <NavButton text="Skills" ref="skills" />
